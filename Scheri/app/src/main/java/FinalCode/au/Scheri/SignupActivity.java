6aab545e6e2b4457e776f531265ff0438890307a
Kenobi101 <michael_gratia21@yahoo.com>
2023-07-24 09:27:17 +0700

    Fix Login & Signup button

--- Scheri/app/src/main/java/FinalCode/au/Scheri/SignupActivity.java
+++ Scheri/app/src/main/java/FinalCode/au/Scheri/SignupActivity.java
@@ -1,6 +1,9 @@
 package FinalCode.au.Scheri;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 import androidx.annotation.Nullable;
@@ -10,9 +13,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import FinalCode.au.Scheri.R;
 
 public class SignupActivity extends AppCompatActivity {
+    private Button button;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        button = (Button) findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logInPage();
+            }
+        });
+
+    }
+
+    public  void logInPage(){
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
     }
 }

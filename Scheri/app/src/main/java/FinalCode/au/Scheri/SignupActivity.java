92b667d979fdb4f3544ef57bc12a681289c03360
Kenobi101 <michael_gratia21@yahoo.com>
2023-07-24 20:22:44 +0700

    Add name, user, password for database

--- Scheri/app/src/main/java/FinalCode/au/Scheri/SignupActivity.java
+++ Scheri/app/src/main/java/FinalCode/au/Scheri/SignupActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 import androidx.annotation.Nullable;
@@ -14,6 +15,7 @@ import FinalCode.au.Scheri.R;
 
 public class SignupActivity extends AppCompatActivity {
     Button button;
+    Button buttonRegister;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +28,22 @@ public class SignupActivity extends AppCompatActivity {
                 logInPage();
             }
         });
+
+        buttonRegister = (Button) findViewById(R.id.register);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText userIdEditText = (EditText) findViewById(R.id.userId);
+                String userId = userIdEditText.getText().toString();
+
+                EditText nameEditText = (EditText) findViewById(R.id.name);
+                String name = nameEditText.getText().toString();
+
+                EditText passwordEditText = (EditText) findViewById(R.id.password);
+                String password = passwordEditText.getText().toString();
+            }
+        });
+
 
     }
 

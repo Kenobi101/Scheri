62c36a48ebc1956ab1e666888e7d9c0f601b9cd9
Kenobi101 <michael_gratia21@yahoo.com>
2023-07-27 16:00:28 +0700

    Removing time picker and adding alarmManager

--- Scheri/app/src/main/java/FinalCode/au/Scheri/AlarmReceiver.java
+++ Scheri/app/src/main/java/FinalCode/au/Scheri/AlarmReceiver.java
@@ -6,11 +6,19 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.provider.Settings;
 
+import androidx.core.app.NotificationCompat;
+
 public class AlarmReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent){
 
-
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "Scheri Application")
+        .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Schedule Notice")
+                .setContentText("You have a Schedule coming")
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
 
     }
 }

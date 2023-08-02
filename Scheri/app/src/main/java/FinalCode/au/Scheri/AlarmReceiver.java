15e537d19c1e2fceae3466a2a0712f0248d09145
Kenobi101 <michael_gratia21@yahoo.com>
2023-08-02 15:01:12 +0700

    Set Alarm sudah bisa time picker tapi langsung return ke Event Edit

--- Scheri/app/src/main/java/FinalCode/au/Scheri/AlarmReceiver.java
+++ Scheri/app/src/main/java/FinalCode/au/Scheri/AlarmReceiver.java
@@ -3,12 +3,11 @@ package FinalCode.au.Scheri;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import androidx.core.app.NotificationCompat;
 
 import FinalCode.au.Scheri.R;
 
 
-public class AlarmReceiver<NotificationHelper> extends BroadcastReceiver {
+public class AlarmReceiver extends BroadcastReceiver {
 
 
     @Override
